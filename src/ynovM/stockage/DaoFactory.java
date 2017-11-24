9e340dcae71cbb5d74c227970a82aeed066247a4
Thomas Alunni-Gradini <thomas.alunni-gradini@ynov.com>
2017-11-24 15:00:29 +0100

    DaoJPA & Factory done (a priori) & added comments

--- src/ynovM/stockage/DaoFactory.java
+++ src/ynovM/stockage/DaoFactory.java
@@ -1,17 +1,74 @@
 package ynovM.stockage;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * 
+ * @author Tomafa06
+ * Classe Singleton
+ */
 public class DaoFactory {
 
+	/**
+	 * Nom du fichier de propriétés
+	 */
+	public final static String FICHIER="acces.properties";
+	
+	/**
+	 * Instance de la factory
+	 */
 	private static DaoFactory instance = null;
-	private DaoFactory() {
-
-	}
+	
+	/**
+	 * ctor PRIVÉ
+	 */
+	private DaoFactory() {}
+	
+	/**
+	 * 
+	 * @return l'instance de la factory, si elle n'existe pas, 
+	 * la crée puis la retourne
+	 */
 	public static DaoFactory getInstance() {
 		if (instance == null)
 		{
-			instance=new DaoFactory();
+			instance = new DaoFactory();
 		}
 		return instance;
 	}
 
+	/**
+	 * 
+	 * @return une nouvelle DaoJPA
+	 */
+	public Dao<StationPOJO> getDao() {
+		return new DaoJPA();
+	}
+	
+	/**
+	 * 
+	 * @return le nom de la DaoJPA grâce à l'injection de dépendance
+	 */
+	private String getNomDao()
+	{
+		String ret = "";
+		Properties prop = null;
+		prop = new Properties();
+		
+		try {
+			prop.load(new FileInputStream(FICHIER));
+			ret = prop.getProperty("package") + prop.getProperty("dao");
+		} catch (FileNotFoundException e) {
+			// FIXME Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return ret;
+	}
+	
 }
--- src/ynovM/stockage/DaoJPA.java
+++ src/ynovM/stockage/DaoJPA.java
@@ -6,38 +6,66 @@ import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 import javax.persistence.PersistenceContext;
 
-import ynovM.Stockage.Dao;
+import ynovM.stockage.Dao;
 import ynovM.service.StationPOJO;
 
+/**
+ * 
+ * @author Tomafa06
+ *	
+ *	Classe faisant le lien entre les objets JAVA 
+ *	et la BDD pour sauvegarder les données
+ */
 public class DaoJPA implements Dao<StationPOJO> {
 
 	@PersistenceContext(unitName="ynovm")
+	
+	/**
+	 * Permet d'assurer toutes les actions de la persistance
+	 */
 	private EntityManager em;
 	
+	/**
+	 * ctor
+	 * initialise l'EntityManager
+	 */
 	public DaoJPA() {
 		em = Persistence.createEntityManagerFactory("ynovm").createEntityManager();
 	}
 	
+	/**
+	 * 
+	 * @return la liste de toutes les StationPOJO
+	 */
 	@Override
 	public List<StationPOJO> lireTous() {
 		return em.createNamedQuery("lireTous",StationPOJO.class).getResultList();
 	}
 	
+	/**
+	 * 
+	 * @param cle
+	 * @return la StationPOJO demandée grâce à la clé (index)
+	 */
 	@Override
-	public CagePOJO lire(int cle) {
+	public StationPOJO lire(int cle) {
 		return em.find(StationPOJO.class, cle);
 	}
 	
+	/**
+	 * insère une nouvelle StationPOJO dans la base de données
+	 * @param elt
+	 */
 	@Override
 	public void inserer(StationPOJO elt) {
 		em.persist(elt);
 	}
 	
-	@Override
-	public void update(int index, StationPOJO obj) {
-		update(obj);
-	}
-	
+	/**
+	 * supprime une stationPOJO de la base de données
+	 * en fonction de la clé (index)
+	 * @param cle
+	 */
 	@Override
 	public void effacer(int cle) {
 		StationPOJO tmp = null;
@@ -45,6 +73,15 @@ public class DaoJPA implements Dao<StationPOJO> {
 		em.remove(tmp);
 	}
 	
+	@Override
+	public void update(int index, StationPOJO obj) {
+		update(obj);
+	}
+	
+	/**
+	 * met à jour une StationPOJO dans la base de données
+	 * @param obj
+	 */
 	@Override
 	public void update(StationPOJO obj) {
 		em.merge(obj);
