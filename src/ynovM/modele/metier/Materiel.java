4e8bcc795033f99171f70501379b66b75d146985
Adrien Canino <thomas.alunni-gradini@y-nov.com>
2017-11-24 16:40:19 +0100

    getters and setters

--- src/ynovM/modele/metier/Materiel.java
+++ src/ynovM/modele/metier/Materiel.java
@@ -15,4 +15,52 @@ public class Materiel {
 	private String reference;
 	private String type;
 	private Date date;
+	/**
+	 * @return the nom
+	 */
+	public String getNom() {
+		return nom;
+	}
+	/**
+	 * @param nom the nom to set
+	 */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	/**
+	 * @return the reference
+	 */
+	public String getReference() {
+		return reference;
+	}
+	/**
+	 * @param reference the reference to set
+	 */
+	public void setReference(String reference) {
+		this.reference = reference;
+	}
+	/**
+	 * @return the type
+	 */
+	public String getType() {
+		return type;
+	}
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+	/**
+	 * @return the date
+	 */
+	public Date getDate() {
+		return date;
+	}
+	/**
+	 * @param date the date to set
+	 */
+	public void setDate(Date date) {
+		this.date = date;
+	}
 }
