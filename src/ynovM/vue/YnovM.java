3dcbcf3c07b4414a4d5f744aaede261d599ef9d1
Thomas Palazzetti <thomas.palazzetti@ynov.com>
2017-11-25 19:13:27 +0100

    Implementation des methodes de recherche de manager dans la class YnovM.

--- src/ynovM/vue/YnovM.java
+++ src/ynovM/vue/YnovM.java
@@ -3,7 +3,9 @@ package ynovM.vue;
 import java.util.List;
 
 import ynovM.controleur.Manager;
-import ynovM.modele.metier.Station;
+import ynovM.modele.technique.StationException;
+import ynovM.utilitaire.EtatStation;
+import ynovM.utilitaire.TypeStation;
 
 public class YnovM {
 
@@ -20,23 +22,43 @@ public class YnovM {
 		Manager.getInstance().supprimer(id);
 	}
 	
-	public void rechercher() {
-		
+	public void redemarrer(int id) throws StationException {
+		Manager.getInstance().redemarrer(id);
+	}
+	
+	public String rechercherParID(int id) throws StationException {
+		return Manager.getInstance().getStationById(id);
+	}
+	
+	public List<String> rechercherParNom(String name) throws StationException {
+		return Manager.getInstance().getStationsByName(name);
+	}
+	
+	public List<String> rechercherParLoc(String loc) throws StationException {
+		return Manager.getInstance().getStationsByLocalisation(loc);
+	}
+	
+	public List<String> getStationsByEtat(EtatStation etat) throws StationException {
+		return Manager.getInstance().getStationsByEtat(etat);
+	}
+	
+	public List<String> getStationsByType(TypeStation type) throws StationException {
+		return Manager.getInstance().getStationsByType(type);
 	}
 	
 	public static void main(String[] args) {
 		YnovM z = null;
 		z = new YnovM();
 					
-		System.out.println("-----------------------------");
+		System.out.println("-------------AFFICHAGE---------------");
 		z.afficher();
-		System.out.println("-----------------------------");
+		System.out.println("-------------AJOUT---------------");
 		z.ajout(5, 7, 8, "AlphaStation", "Bretagne", 17, 4, 5, 8, 7, "RAS");
 		System.out.println("---------------APRES AJOUT-------------");
 		z.afficher();
-		System.out.println("-----------------------------");
+		System.out.println("----------------SUPPRIMER-------------");
 		z.supprimer(1);
-		System.out.println("-----------------------------");
+		System.out.println("------------------APRES SUPPRIMER-----------");
 		z.afficher();
 		System.out.println("-----------------------------");
 	}
