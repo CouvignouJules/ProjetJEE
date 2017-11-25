777d8a0e0eeb36d232f8f979a8f2e31eb09b3def
Alex <aleqsd@gmail.com>
2017-11-25 13:03:50 +0100

    ProfileException

---
+++ src/ynovM/modele/technique/ProfileException.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package ynovM.modele.technique;
+
+/**
+ * @author Alex
+ *
+ */
+public class ProfileException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ProfileException()
+	{
+		super("Ce type de profile n'est pas autorisé à faire ceci.");
+	}
+	public ProfileException(String msg)
+	{
+		super(msg);
+	}
+}
